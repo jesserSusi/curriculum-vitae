87b34a5ad9d4fde2346627040de6e6ce11c7d24d
Jesser Susi <susi.jesser@.com>
2020-07-27 15:51:04 +1200

    Updated layout for most mobile devices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
@@ -8,31 +8,56 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Sidebar from "./components/Sidebar";
 
-function App() {
-  return (
-    <div className="main">
-      <Container fluid>
-        <Row>
-          <Col xl={1} />
-          <Col sm={12} xs={12} md={12} lg={3} xl={2}>
-            <Sidebar />
-          </Col>
-          <Col>
-            <div className="main-scroll">
-              <About />
-              <hr />
-              <Skills />
-              <hr />
-              <Experience />
-              <hr />
-              <Projects />
-            </div>
-          </Col>
-          <Col xl={1} />
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideHR: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  resize() {
+    let currentHideNav = window.innerWidth <= 960;
+    if (currentHideNav !== this.state.hideNav) {
+      this.setState({ hideHR: currentHideNav });
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resize.bind(this));
+  }
+  render() {
+    return (
+      <div className="main">
+        <Container fluid>
+          <Row>
+            <Col xl={1} />
+            <Col sm={12} xs={12} md={12} lg={3} xl={2}>
+              <Sidebar />
+            </Col>
+            <Col>
+              <div className="main-scroll">
+                {this.state.hideHR && <hr />}
+                <About />
+                <hr />
+                <Skills />
+                <hr />
+                <Experience />
+                <hr />
+                <Projects />
+              </div>
+            </Col>
+            <Col xl={1} />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Row from "react-bootstrap/Row";
 import Tooltip from "react-bootstrap/Tooltip";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -25,75 +27,55 @@ const SocialIcon = (props) => {
 };
 
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hideHR: false,
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.resize.bind(this));
-    this.resize();
-  }
-
-  resize() {
-    let currentHideNav = window.innerWidth < 768;
-    if (currentHideNav !== this.state.hideNav) {
-      this.setState({ hideHR: currentHideNav });
-    }
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.resize.bind(this));
-  }
-
   render() {
     return (
       <div className="sidenav">
-        <div className="avatar">
-          <Image src={process.env.REACT_APP_AVATAR} rounded fluid />
-        </div>
-        <div className="sidenav-info">
-          <h1>{process.env.REACT_APP_NAME}</h1>
-          <h5>Software Engineer</h5>
-          <h5>
-            <FontAwesomeIcon icon={faMapMarkerAlt} size="1x" />
-            <a href={process.env.REACT_APP_LOCATION}> Wellington, NZ</a>
-          </h5>
-
-          <div className="socials">
-            <h5 style={{ marginRight: "0.5rem" }}>Connect with me:</h5>
-            <div className="socials-icons">
-              <SocialIcon
-                link={process.env.REACT_APP_FOOTER_GITHUB}
-                classname="github social"
-                faicon={faGithub}
-                info="GitHub"
-              />
-              <SocialIcon
-                link={process.env.REACT_APP_FOOTER_LINKEDIN}
-                classname="linkedin social"
-                faicon={faLinkedin}
-                info="LinkedIn"
-              />
-              <SocialIcon
-                link={process.env.REACT_APP_FOOTER_EMAIL}
-                classname="email social"
-                faicon={faEnvelope}
-                info="Email"
-              />
-              <SocialIcon
-                link={process.env.REACT_APP_FOOTER_CV}
-                classname="resume social"
-                faicon={faFileCode}
-                info="Curriculum Vitae or Resume"
-              />
+        <Row>
+          <Col sm={4} md={4} lg={12} xl={12}>
+            <div className="avatar">
+              <Image src={process.env.REACT_APP_AVATAR} rounded fluid />
             </div>
-          </div>
-        </div>
+          </Col>
+          <Col>
+            <div className="sidenav-info">
+              <h1>{process.env.REACT_APP_NAME}</h1>
+              <h5>Software Engineer</h5>
+              <h5>
+                <FontAwesomeIcon icon={faMapMarkerAlt} size="1x" />
+                <a href={process.env.REACT_APP_LOCATION}> Wellington, NZ</a>
+              </h5>
 
-        {this.state.hideHR && <hr />}
+              <div className="socials"></div>
+              <h5 style={{ marginRight: "0.5rem" }}>Connect with me:</h5>
+              <div className="socials-icons">
+                <SocialIcon
+                  link={process.env.REACT_APP_FOOTER_GITHUB}
+                  classname="github social"
+                  faicon={faGithub}
+                  info="GitHub"
+                />
+                <SocialIcon
+                  link={process.env.REACT_APP_FOOTER_LINKEDIN}
+                  classname="linkedin social"
+                  faicon={faLinkedin}
+                  info="LinkedIn"
+                />
+                <SocialIcon
+                  link={process.env.REACT_APP_FOOTER_EMAIL}
+                  classname="email social"
+                  faicon={faEnvelope}
+                  info="Email"
+                />
+                <SocialIcon
+                  link={process.env.REACT_APP_FOOTER_CV}
+                  classname="resume social"
+                  faicon={faFileCode}
+                  info="Curriculum Vitae or Resume"
+                />
+              </div>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
